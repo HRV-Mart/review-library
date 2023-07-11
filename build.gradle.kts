41ea4ce5d4a99050c4fe6afbd5a14d5898679dc4
Harsh3305 <harshverma3305@gmail.com>
2023-07-11 12:01:23 +0530

    add  api-call library

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.0")
 	// HRV-Mart
 	implementation("com.hrv.mart:user-library:0.0.3")
+	implementation("com.hrv.mart:api-call:0.0.3")
 }
 
 tasks.withType<KotlinCompile> {
